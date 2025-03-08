528fc494cee452d87d6d0128bc1549d2f80927e7
Mohammad Qady <112632549+MOQ2@users.noreply.github.com>
2025-03-08 20:49:39 +0200

    Refactoring: change the dog_names from list to dic for faster serach .

--- intropyproject-classify-pet-images/adjust_results4_isadog.py
+++ intropyproject-classify-pet-images/adjust_results4_isadog.py
@@ -69,11 +69,10 @@ def adjust_results4_isadog(results_dic, dogfile):
     """           
     
     # read the dog names from the file
-    doge_names = []
+    doge_names = {}
     with open(dogfile, 'r') as file:
-        for line in file:
-            doge_names.append(line.strip())
-            
+      for line in file:
+        doge_names[line.strip()] = 1
     for key in results_dic:
         # check if pet image label is a dog
         results_dic[key].append(1 if results_dic[key][0] in doge_names else 0)
